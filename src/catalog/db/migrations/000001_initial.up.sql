5787fc8cc2349505948e3315eb1f67bea439f99f
Scott Lowe <slowe@pulumi.com>
2023-07-07 15:35:08 -0600

    Initial changes to rebrand
    Update assets to include new images. Change DB migration file.
    
    Signed-off-by: Scott Lowe <slowe@pulumi.com>

--- src/catalog/db/migrations/000001_initial.up.sql
+++ src/catalog/db/migrations/000001_initial.up.sql
@@ -24,12 +24,12 @@ CREATE TABLE IF NOT EXISTS product_tag (
 		REFERENCES tag(tag_id)
 );
 
-INSERT INTO product VALUES ("6d62d909-f957-430e-8689-b5129c0bb75e", "Pocket Watch", "Properly dapper.", 385, 33, "/assets/pocket_watch.jpg");
-INSERT INTO product VALUES ("a0a4f044-b040-410d-8ead-4de0446aec7e", "Wood Watch", "Looks like a tree", 50, 115, "/assets/wood_watch.jpg");
-INSERT INTO product VALUES ("808a2de1-1aaa-4c25-a9b9-6612e8f29a38", "Chronograf Classic", "Spend that IPO money",  5100, 9, "/assets/chrono_classic.jpg");
-INSERT INTO product VALUES ("510a0d7e-8e83-4193-b483-e27e09ddc34d", "Gentleman", "Touch of class for a bargain.",  795, 51, "/assets/gentleman.jpg");
-INSERT INTO product VALUES ("ee3715be-b4ba-11ea-b3de-0242ac130004", "Smart 3.0", "Can tell you what you want for breakfast",  650, 9, "/assets/smart_1.jpg");
-INSERT INTO product VALUES ("f4ebd070-b4ba-11ea-b3de-0242ac130004", "FitnessX", "Touch of class for a bargain.",  180, 76, "/assets/smart_2.jpg");
+INSERT INTO product VALUES ("6d62d909-f957-430e-8689-b5129c0bb75e", "Arcane Staff", "Properly dapper.", 385, 33, "/assets/staff.png");
+INSERT INTO product VALUES ("a0a4f044-b040-410d-8ead-4de0446aec7e", "Globe", "Looks like a tree", 50, 115, "/assets/globe.png");
+INSERT INTO product VALUES ("808a2de1-1aaa-4c25-a9b9-6612e8f29a38", "Anti-Gravity Saucer", "Spend that IPO money",  5100, 9, "/assets/anti-grav-saucer.png");
+INSERT INTO product VALUES ("510a0d7e-8e83-4193-b483-e27e09ddc34d", "Portal Gun", "Touch of class for a bargain.",  795, 51, "/assets/portal-gun.png");
+INSERT INTO product VALUES ("ee3715be-b4ba-11ea-b3de-0242ac130004", "Power Glove", "Can tell you what you want for breakfast",  650, 9, "/assets/glove.png");
+INSERT INTO product VALUES ("f4ebd070-b4ba-11ea-b3de-0242ac130004", "Flux Capacitor", "Touch of class for a bargain.",  1800, 76, "/assets/flux-capacitor.png");
 
 INSERT INTO tag (name, display_name) VALUES ("smart", "Smart");
 INSERT INTO tag (name, display_name) VALUES ("dress", "Dress");
